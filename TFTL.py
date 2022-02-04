e453dbd952ec97f00d52b02dc408d0c0d9c9a336
SanctiMagistri <wasylukmateusz@gmail.com>
2022-02-04 13:27:44 +0100

    Added window frame

--- TFTL.py
+++ TFTL.py
@@ -2,5 +2,28 @@
 Generator postaci bohaterów do gry fabularnej Tales From The Loop
 Autor: Mateusz Wasyluk
 '''
-print("Hello world!")
-print("Check")
+
+from tkinter import *
+from tkinter.ttk import *
+
+class Window(Frame):
+    def __init__(self,parent):
+        super().__init__(parent)
+
+        self.style = Style()
+        self.parent = parent
+        self.initialize()
+
+    def initialize(self):
+        self.parent.title("TFTL Generator Postaci")
+        self.style.theme_use('clam')
+        self.style.configure('.', font=('Helvetica', 12))
+
+def main():
+    gui = Tk()
+    gui.geometry('1280x720')
+    Window(gui)
+    gui.mainloop()
+
+if __name__ == '__main__':
+    main()
